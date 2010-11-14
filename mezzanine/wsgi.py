b94f2d01b1fd2abd6b982751b68e0ab8678c1167
Patrick J. McNerthney <pmcnerthney@clearpointmetrics.com>
2010-11-13 15:54:47 -1000

    Point to the mezzanine settings file.

--- mezzanine/wsgi.py
+++ mezzanine/wsgi.py
@@ -3,5 +3,5 @@ import _setup # Must be first
 import django.core.handlers.wsgi
 import os
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'storm.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mezzanine.settings'
 application = django.core.handlers.wsgi.WSGIHandler()
