a86ebf9dd9340ec92e42003d7065bb20e4f0f896
Patrick J. McNerthney <pmcnerthney@clearpointmetrics.com>
2010-11-25 11:55:46 -1000

    Get grappelli under the /admin/ url.

--- mezzanine/urls.py
+++ mezzanine/urls.py
@@ -10,13 +10,13 @@ handler404 = 'mezzanine.core.views.handler_404'
 handler500 = 'mezzanine.core.views.handler_500'
 
 urlpatterns = patterns("",
-    url("^admin/page_ordering/$", "mezzanine.pages.views.admin_page_ordering",
-        name="admin_page_ordering"),
     url("^admin/keywords_submit/$", "mezzanine.core.views.admin_keywords_submit",
         name="admin_keywords_submit"),
+    url("^admin/page_ordering/$", "mezzanine.pages.views.admin_page_ordering",
+        name="admin_page_ordering"),
     ("^admin/filebrowser/", include("filebrowser.urls")),
+    ("^admin/grappelli/", include("grappelli.urls")),
     ("^admin/", include(admin.site.urls)),
-    ("^grappelli/", include("grappelli.urls")),
     ("^", include("mezzanine.core.urls")),
     ("^", include("mezzanine.pages.urls")),
 )
