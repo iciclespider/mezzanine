a3c7a20abdb112ed02c5862debc790d4a5492826
Patrick J. McNerthney <pmcnerthney@clearpointmetrics.com>
2010-11-26 09:42:45 -1000

    Add in the settings.py change to use "staff", rather than "films".

--- mezzanine/settings.py
+++ mezzanine/settings.py
@@ -125,7 +125,7 @@ INSTALLED_APPS = (
     #"mezzanine.blog",
     #"mezzanine.forms",
     #"mezzanine.twitter",
-    "mezzanine.films",
+    "mezzanine.staff",
     "grappelli",
     "filebrowser",
     "django.contrib.admin",
